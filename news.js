a3f63969980065bb5b690e1942ebdf9af426e3d3
Narottam <coutkunal@gmail.com>
2024-07-20 13:48:24 +0530

    testing out new package that also provides images

--- news.js
+++ news.js
@@ -1,15 +1,33 @@
-const googleNewsAPI = require("google-news-json");
+// const googleNewsAPI = require("google-news-json");
+const googleNewsScraper = require("google-news-scraper");
+
 const fs = require("fs");
 
 async function fetchNews() {
   try {
-    const news = await googleNewsAPI.getNews(
-      googleNewsAPI.SEARCH,
-      "cryptocurrency"
-    );
-    const newsData = JSON.stringify(news.items, null, 2);
+    // const news = await googleNewsAPI.getNews(
+    //   googleNewsAPI.SEARCH,
+    //   "cryptocurrency"
+    // );
+    // const newsData = JSON.stringify(news.items, null, 2);
+    // fs.writeFileSync("news.json", `${newsData}`);
+
+    const articles = await googleNewsScraper({
+      searchTerm: "Cryptocurrency",
+      prettyURLs: false,
+      queryVars: {
+        hl: "en-US",
+        gl: "US",
+        ceid: "US:en",
+      },
+      timeframe: "24h",
+      puppeteerArgs: [],
+    });
+
+    const newsData = JSON.stringify(articles, null, 2);
     fs.writeFileSync("news.json", `${newsData}`);
-    console.log("News updated successfully", newsData);
+
+    console.log("News updated successfully", newsData.length, newsData);
   } catch (error) {
     console.error("Error fetching news:", error);
     process.exit(1);
